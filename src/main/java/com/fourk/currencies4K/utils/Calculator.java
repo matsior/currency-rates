ad26f3afd92a67461811b16d1447cb9653f0e0a1
Mateusz Krajewski <maatsior@gmail.com>
2022-07-07 10:23:45 +0200

    edit Calculator.java

--- src/main/java/com/fourk/currencies4K/utils/Calculator.java
+++ src/main/java/com/fourk/currencies4K/utils/Calculator.java
@@ -2,7 +2,12 @@ package com.fourk.currencies4K.utils;
 
 public class Calculator {
 
+    public static double roundAvoid(double calculate, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(calculate * scale) / scale;
+    }
+
     public double calculate(double base, double target, double amount) {
-        return base * amount / target;
+        return roundAvoid(base * amount / target, 3);
     }
 }
